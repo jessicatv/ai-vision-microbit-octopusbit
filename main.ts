f8687ffabcf37c17f3efc7560f8df32ed29c2e71
Jessicatv <2615373+jessicatv@users.noreply.github.com>
2021-02-21 21:05:53 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,19 @@
 input.onButtonPressed(Button.B, function () {
     PlanetX_AILens.initModule()
-    PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Ball)
-    PlanetX_AILens.cameraImage()
-    if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Red)) {
-        music.playMelody("E D C G - - - - ", 120)
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-    }
+    PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Color)
 })
 PlanetX_AILens.initModule()
-PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Ball)
+PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Color)
 basic.forever(function () {
     PlanetX_AILens.cameraImage()
-    if (PlanetX_AILens.ballColor(PlanetX_AILens.ballColorList.Red)) {
-        music.playMelody("E D C G - - - - ", 120)
+    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.yellow)) {
+        music.playMelody("A G - - - - - - ", 120)
         basic.showLeds(`
             . . . . .
             . # . # .
-            # . . . #
+            # # . # #
             . # . # .
-            . . # . .
+            . . . . .
             `)
     }
 })
